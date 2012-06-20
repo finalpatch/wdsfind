8e644225efd3c3b557be976276af8652dc3db8ef
finalpatch <fengli@gmail.com>
2012-06-21 01:13:38 +1000

    initial checkin

---
+++ wdsfind.fs
@@ -0,0 +1,27 @@
+open System.Data
+open System.Data.OleDb
+open System.Runtime.InteropServices
+
+let queryWDS term =
+    let conn = new OleDbConnection("Provider=Search.CollatorDSO.1;Extended Propertes=\"Application=Windows\"")
+    do conn.Open()
+    let q = sprintf "SELECT Top 100 System.ItemPathDisplay FROM \
+                     SYSTEMINDEX WHERE FREETEXT('%s')" term
+    let cmd = new OleDbCommand(q, conn)
+    cmd.ExecuteReader()
+    |> Seq.cast<IDataRecord>
+    |> Seq.map (fun o -> o.GetString 0)
+
+[<DllImport("kernel32.dll")>]
+extern bool SetConsoleOutputCP(uint32 wCodePageID);
+
+[<EntryPointAttribute>]
+let main args =
+    SetConsoleOutputCP 65001u |> ignore
+    if (Array.length args) >= 1 then
+        queryWDS args.[0]
+        |> Seq.map (fun s -> s.Replace("\\", "/"))
+        |> Seq.iter (printfn "%s")
+    else
+        printfn "Usage: wdsfind term"
+    0
